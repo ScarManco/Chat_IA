b1666a2b195166e04b5a4376c085549970bb85bb
ScarManco <135183926+ScarManco@users.noreply.github.com>
2025-08-05 12:30:54 -0600

    Remove Watson Assistant completely

--- src/components/chat/WatsonAssistant.tsx
+++
@@ -1,6 +0,0 @@
-// Watson Assistant component disabled due to invalid integration credentials
-// To re-enable, update with valid integrationID and serviceInstanceID from IBM Cloud
-
-export function WatsonAssistant() {
-  return null;
-}
--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import { Sidebar } from './Sidebar';
-import { WatsonAssistant } from '../chat/WatsonAssistant';
 
 export function Layout() {
   return (
@@ -10,7 +9,6 @@ export function Layout() {
       <main className="flex-1 overflow-auto p-8">
         <Outlet />
       </main>
-      <WatsonAssistant />
     </div>
   );
 }
